d5ff05dd7e23810f32f1702c075f0a0df9579248
Antonio Seraus <aseraus@chello.nl>
2025-07-14 19:07:29 +0200

    remote webdriver added

--- src/test/java/tests/LoginBasicTest.java
+++ src/test/java/tests/LoginBasicTest.java
@@ -3,6 +3,8 @@ package tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -12,6 +14,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 public class LoginBasicTest {
@@ -19,9 +23,10 @@ public class LoginBasicTest {
     WebDriver driver;
 
     @BeforeMethod
-    public void setUp(){
+    public void setUp() throws MalformedURLException {
 
-        driver = new ChromeDriver();
+        //driver = new ChromeDriver();
+        driver = new RemoteWebDriver(new URL("http://localhost:4444"),new ChromeOptions());
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
     }
@@ -42,8 +47,8 @@ public class LoginBasicTest {
                 {"visual_user"}
         };
     }
+//docker run -d -p 4444:4444 -p 7900:7900 --shm-size="2g" --name browser-container selenium/standalone-chrome:lates
     @Test(dataProvider = "userAccounts")
-
     public void loginTest(String username){
           driver.get("https://www.saucedemo.com/");
           Assert.assertTrue(driver.getTitle().equals("Swag Labs"));
